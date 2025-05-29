b45848747ea7cace66b7b628fa94b832e6e093bf
danilomartinelli <danilomartinelli429@gmail.com>
2025-05-29 11:24:32 -0600

    Adds check for existing Bitwarden CLI install
    Prevents unnecessary reinstallation of the Bitwarden CLI by checking if 'bws' command is already available in the system path before attempting installation

--- secret-manager/install.sh
+++ secret-manager/install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-curl https://bws.bitwarden.com/install | sh
+if ! command -v bws >/dev/null 2>&1
+then
+  curl https://bws.bitwarden.com/install | sh
+fi
 
 echo "✓ Bitwarden CLI installed"
