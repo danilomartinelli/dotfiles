6f567869c968138091c95ab97c69382dc644ee9f
Danilo Martinelli <danilomartinelli429@gmail.com>
2026-08-21 09:42:45 -0300

    Fix eza aliases for positional paths

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,10 @@
 # Modern file listing: prefer eza, fall back to GNU coreutils gls.
 if (( $+commands[eza] )); then
-  alias ls="eza --icons"
-  alias l="eza -lAh --icons"
-  alias ll="eza -l --icons"
-  alias la='eza -A --icons'
-  alias lt='eza --tree --icons'
+  alias ls="eza --icons=auto"
+  alias l="eza -lAh --icons=auto"
+  alias ll="eza -l --icons=auto"
+  alias la='eza -A --icons=auto'
+  alias lt='eza --tree --icons=auto'
 elif (( $+commands[gls] )); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
